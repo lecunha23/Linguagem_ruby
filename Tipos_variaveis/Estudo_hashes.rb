f8944bfdfb75d4d543e8c97e08c3a49878e724c1
leandrocunha <leandrojc23@gmail.com>
2024-02-04 20:01:15 -0300

    Continuando os estudos

---
+++ Tipos_variaveis/Estudo_hashes.rb
@@ -0,0 +1,10 @@
+#usando metodo hashes
+
+pessoa = {
+  "nome" => "Leandro",
+  "sobrenome" => "Cunha",
+  "idade" => "34"
+
+}
+puts "Nome: #{pessoa["nome"]} #{pessoa["sobrenome"]}"
+puts "Idade: #{pessoa["idade"]}"
