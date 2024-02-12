4354bd71cd94d1dfc48a8794bb1a8dee3f144404
leandrocunha <leandrojc23@gmail.com>
2024-02-12 07:05:51 -0300

    teste1

--- tarefaAlgoritimo/LinearSearch.rb
+++ tarefaAlgoritimo/LinearSearch.rb
@@ -9,16 +9,17 @@ def linear_search(array, target)
     end
   end
 
-  return nill
+  return nil
   #retorna nil se o alvo nao for encontrado
 end
 
 arr = [10, 20, 30, 80, 50, 60, 70]
-target = 50
+target = 25
+
 result = linear_search(arr, target)
 if result
   puts "Elemento encontrado #{target} no indice #{result}."
 
 else
-  puts "Elemento nao encontrado#{target} nos array."
+  puts "Elemento #{target} nao encontrado nos array."
 end
